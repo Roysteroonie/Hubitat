e0c6ab79fe2a08b35a45e6c38f8a82d62d2c2b42
Roysteroonie <roy.knowles@me.com>
2019-05-29 11:37:30 +0100

    Update GreenWave PowerNode (Single) Advanced - HE.groovy

--- GreenWave PowerNode (Single) Advanced - HE.groovy
+++ GreenWave PowerNode (Single) Advanced - HE.groovy
@@ -29,7 +29,8 @@
  *  Updated for HE by @Royski 28/05/19
  *****************************************************************************************************************/
 metadata {
-    definition (name: "GreenWave PowerNode (Single) Advanced - HE", namespace: "codersaur", author: "David Lomas", importUrl: "https://raw.githubusercontent.com/Roysteroonie/Hubitat/master/GreenWave%20PowerNode%20(Single)%20Advanced%20-%20HE.groovy") {
+    definition (name: "GreenWave PowerNode (Single) Advanced - HE", namespace: "codersaur", author: "David Lomas", 
+	importUrl: "https://raw.githubusercontent.com/Roysteroonie/Hubitat/master/GreenWave%20PowerNode%20(Single)%20Advanced%20-%20HE.groovy") {
         capability "Actuator"
         capability "Switch"
         capability "Sensor"
