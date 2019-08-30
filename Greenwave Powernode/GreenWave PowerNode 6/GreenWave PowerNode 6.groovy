ef0881f9257bba5aae2062d8d9aec5662f82daa3
Roysteroonie <roy.knowles@me.com>
2019-08-30 19:26:14 +0100

    Update GreenWave PowerNode 6.groovy

--- Greenwave Powernode/GreenWave PowerNode 6/GreenWave PowerNode 6.groovy
+++ Greenwave Powernode/GreenWave PowerNode 6/GreenWave PowerNode 6.groovy
@@ -265,6 +265,7 @@ def poll() {
 
 def pollNodes() {
 	if (logEnable) log.debug "Polling Powerstrip - ${device.label} node ${node}"
+	if (txtEnable) log.info "Polling Powerstrip"
 	def cmds = []
 	for ( i in 1..6 ) { 
 		cmds << command(encap(zwave.switchBinaryV1.switchBinaryGet(), i))
@@ -278,7 +279,7 @@ def pollNodes() {
 }
 
 def pollNode(endpoint)  {
-	if (logEnable) log.debug "Polling ${device.label} node ${endpoint} "
+	if (txtEnable) log.info "Polling Powerstrip node ${endpoint}"
 	def cmds = []
 	cmds << command(encap(zwave.switchBinaryV1.switchBinaryGet(),endpoint))
 	cmds << command(encap(zwave.meterV2.meterGet(scale:0),endpoint))
